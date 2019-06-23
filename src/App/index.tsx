3f42b6585c76def8daa37e8fecae7fc19034d5c1
Yuriy Provotorov <prowotorow.yuriy@gmail.com>
2019-06-23 13:45:40 +0300

    add start kit

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from 'core/store';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">HEADER</header>
-    </div>
+    <Provider store={store}>
+      <div>HELLO</div>
+    </Provider>
   );
 };
 
