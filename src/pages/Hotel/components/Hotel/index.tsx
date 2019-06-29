e5ad1f4701096e0c4626483e85bd7da341955085
Yuriy Provotorov <prowotorow.yuriy@gmail.com>
2019-06-29 15:29:19 +0300

    Add some styles

--- src/pages/Hotel/components/Hotel/index.tsx
+++ src/pages/Hotel/components/Hotel/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-
 import { Container } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
 import { IHotel } from 'interfaces/state/hotels';
+import { styles } from './styles';
+
 interface Props {
   hotel: IHotel;
 }
-const Hotel: React.FC<Props> = ({ hotel }) => {
-  // there is could be another way for passing id (redux or new context api e.g.)
-
+const Hotel: React.FC<Props> = (props) => {
+  const classes = styles(props);
+  const { hotel } = props;
   return (
     <div className="Hotel">
       <Container>
-        <h2>{hotel.name}</h2>
+        <Paper>
+          <div className={classes.wrapTitleHotel}>
+            <Typography classes={{ h3: classes.titleHotel }} variant="h3">
+              {hotel.name}
+            </Typography>
+          </div>
+        </Paper>
       </Container>
     </div>
   );
---
+++ src/pages/Hotel/components/Hotel/styles.tsx
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/styles';
+export const styles = makeStyles({
+  wrapTitleHotel: {
+    display: 'flex'
+  },
+  titleHotel: {
+    fontSize: '24px'
+  }
+});
