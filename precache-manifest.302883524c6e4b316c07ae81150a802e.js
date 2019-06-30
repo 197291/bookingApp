f59ede1e41dd5b2e343419044b1a9c0bcf5ca131
Yuriy Provotorov <prowotorow.yuriy@gmail.com>
2019-06-30 19:16:56 +0300

    Updates

---
+++ precache-manifest.302883524c6e4b316c07ae81150a802e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5650f3eea510322cd66569eb259de969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be2d0bee37d10563f306",
+    "url": "/static/css/2.a7c8aab0.chunk.css"
+  },
+  {
+    "revision": "36a5923740bca04ba88e",
+    "url": "/static/css/main.de7223b8.chunk.css"
+  },
+  {
+    "revision": "be2d0bee37d10563f306",
+    "url": "/static/js/2.7aa26b11.chunk.js"
+  },
+  {
+    "revision": "36a5923740bca04ba88e",
+    "url": "/static/js/main.9aa1b322.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bookingApp/precache-manifest.aa8b38442508cdc5ec53f44482939545.js"
+  "/precache-manifest.302883524c6e4b316c07ae81150a802e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bookingApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
