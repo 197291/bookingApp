04e5b457c64416ee3fc058882729e0990689b5e1
Yuriy Provotorov <prowotorow.yuriy@gmail.com>
2019-06-30 17:55:05 +0300

    Add build folder

---
+++ build/precache-manifest.707d20f3fd412f33ab47b4a72e2979cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adefe732a9bed829990f8e22d9bfe00a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be2d0bee37d10563f306",
+    "url": "/static/css/2.a7c8aab0.chunk.css"
+  },
+  {
+    "revision": "2aac315920bbe9813f7f",
+    "url": "/static/css/main.de7223b8.chunk.css"
+  },
+  {
+    "revision": "be2d0bee37d10563f306",
+    "url": "/static/js/2.7aa26b11.chunk.js"
+  },
+  {
+    "revision": "2aac315920bbe9813f7f",
+    "url": "/static/js/main.dc204a95.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
